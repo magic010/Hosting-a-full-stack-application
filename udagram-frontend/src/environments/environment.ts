e08a779cbb4eb97e89a2bdeadd1b8f48025ebf2b
Mostafa <mostafa.udemy1@gmail.com>
2022-05-02 16:16:38 +0200

    updated

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:3000/api/v0'
+  apiHost: 'http://mostafa-udagram.s3-website-us-east-1.amazonaws.com:3000/api/v0'
 };
 
 /*
